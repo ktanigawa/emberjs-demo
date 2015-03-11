6f6217dd886ae6939ed42dca9f1f7171188138b7
Karen Tanigawa <tanigawa.karen@gmail.com>
2015-03-10 21:43:01 -1000

    interactjs snaps and resizes

---
+++ js/demo.js
@@ -0,0 +1,52 @@
+var element = document.getElementById('grid-snap'),
+    x = 0, y = 0;
+
+interact(element)
+  .draggable({
+    snap: {
+      targets: [
+        interact.createSnapGrid({ x: 30, y: 30 })
+      ],
+      range: Infinity,
+      relativePoints: [ { x: 0, y: 0 } ]
+    },
+    inertia: true,
+    restrict: {
+      restriction: element.parentNode,
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+      endOnly: true
+    }
+  })
+  .on('dragmove', function (event) {
+    x += event.dx;
+    y += event.dy;
+
+    event.target.style.webkitTransform =
+    event.target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)';
+  });
+
+
+var offset = { x: 0, y: 0 };
+
+interact('.resize')
+  .resizable({
+    edges: { left: true, right: true, bottom: true, top: true }
+  })
+  .on('resizemove', function (event) {
+    var target = event.target;
+
+    // update the element's style
+    target.style.width  = event.rect.width + 'px';
+    target.style.height = event.rect.height + 'px';
+
+    // translate when resizing from top or left edges
+    offset.x += event.deltaRect.left;
+    offset.y += event.deltaRect.top;
+
+    // target.style.transform = ('translate('
+    //                           + offset.x + 'px,'
+    //                           + offset.y + 'px)');
+
+    // target.textContent = event.rect.width + '×' + event.rect.height;
+  });
